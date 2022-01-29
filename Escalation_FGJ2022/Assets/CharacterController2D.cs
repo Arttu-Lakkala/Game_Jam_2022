5d76feefd863183dc8cbcb1105b9d875b05d68d8
Arttu-Lakkala <60192672+Arttu-Lakkala@users.noreply.github.com>
2022-01-29 16:27:31 +0200

    made enemy attack changesd some stuff around

--- Escalation_FGJ2022/Assets/CharacterController2D.cs
+++ Escalation_FGJ2022/Assets/CharacterController2D.cs
@@ -7,19 +7,19 @@ public class CharacterController2D : MonoBehaviour
 	public float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
 	public float m_MovementSmoothing = .05f;	// How much to smooth out the movement
 	public bool m_AirControl = false;							// Whether or not a player can steer while jumping;
+  public bool m_FacingRight = true;  // For determining which way the player is currently facing.
 	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
 	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
 	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
 	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
   
   
-  public int Health = 100;				// A collider that will be disabled when crouching
   public float runSpeed = 50;     //runspeed
 	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
 	private bool m_Grounded;            // Whether or not the player is grounded.
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 	private Rigidbody2D m_Rigidbody2D;
-	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+	
 	private Vector3 m_Velocity = Vector3.zero;
 
 	[Header("Events")]
--- Escalation_FGJ2022/Assets/StupidAI.cs
+++ Escalation_FGJ2022/Assets/StupidAI.cs
@@ -4,25 +4,68 @@ using UnityEngine;
 
 public class StupidAI : MonoBehaviour
 {
+  
   [SerializeField] private Transform target;
   [SerializeField] private Transform self;
+  
+  public Animator animator;
+	
+	//Where the attack originates
+	public Transform attackPoint;
+  
   public CharacterController2D controller;
+  public float reaction_dist = 1.5f;
+  public float jumpRate = 2f;
+  public float attackRate = 2f;
+  public float attackRange = 0.5f;
+	public int attackDamage = 10;
+  
+  public LayerMask playerLayer;
+  
+  float nextJumpTime = 0f;
+  float nextAttackTime = 0f;
   
   float horizontalMove = 0f;
-  float reaction_dist = 1.5f;
   bool jump = false;
   bool crouch = false;
+  float action_number;
   // Update is called once per frame
-  void start()
+  void Awake()
   {
+    Debug.Log("awake");
+    //iniate pseudo randomnes
     Random.InitState(18);
+    nextJumpTime = Random.Range(2, 20)/jumpRate;
+    nextAttackTime = Random.Range(2, 20)/attackRate;
   }
+  
+  
+  
   
   void Update()
   {
-     
-     // move towards player horizontaly
-      Debug.Log(Random.value);
+      //is it time to jump
+      if(Time.time >= nextJumpTime)
+      {
+        {
+          Debug.Log("jumped");
+          jump = true;
+          nextJumpTime = (Time.time +Random.Range(2, 20)/jumpRate);
+        }
+      }
+      
+      //is it time to attack
+      if(Time.time >= nextAttackTime)
+      {
+        {
+          Debug.Log("attacked");
+          Attack();
+          nextAttackTime = (Time.time +Random.Range(2, 20)/attackRate);
+        }
+      }
+      
+      
+      // move towards player horizontaly
       if((self.position.x - target.position.x)>reaction_dist)
       {
         horizontalMove = -1;
@@ -31,10 +74,31 @@ public class StupidAI : MonoBehaviour
       {
         horizontalMove = 1;
       }
+      else
+      {
+        horizontalMove = 0;
+      }
   }
-  void FixedUpdate ()
+   void FixedUpdate()
   {
     //move char
     controller.Move(horizontalMove,crouch,jump);
-  }      
+    if (jump==true)
+    {
+      jump = false;
+    }
+  }
+  void Attack()
+	{
+		//Play attack anim
+		//animator.SetTrigger("Attack");
+		//Detect enemy in range
+		Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, playerLayer);
+		//Deal damage
+		foreach (Collider2D enemy in hitEnemies)
+		{
+			enemy.GetComponent<CharacterStats>().TakeDamage(attackDamage);
+			Debug.Log("Hit Player");
+		}
+	}  
 }
