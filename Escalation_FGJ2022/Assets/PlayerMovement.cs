5ceef482252df6e254c4b918f57051ee76109b34
Arttu-Lakkala <60192672+Arttu-Lakkala@users.noreply.github.com>
2022-01-29 12:28:55 +0200

    added player movement and jumping

---
+++ Escalation_FGJ2022/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 50;
+    
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    // Update is called once per frame
+    void Update()
+    {
+       //get horisontal movement based on movespeed
+       horizontalMove = Input.GetAxisRaw ("Horizontal")*runSpeed;
+      if(Input.GetAxisRaw ("Vertical")>0)
+      {
+        jump= true;
+        crouch = false;
+      }
+      else if(Input.GetAxisRaw ("Vertical")<0)
+      {
+        crouch = true;
+        jump = false;
+      }
+      else
+      {
+        jump = false;
+        crouch = false;
+      }
+    }
+    
+    void FixedUpdate ()
+    {
+      //move char
+      controller.Move(horizontalMove * Time.fixedDeltaTime,crouch,jump);
+    }
+}
