c1b4569bbbe4e837df4e2a8ebb879dd7f06264e4
Arttu-Lakkala <60192672+Arttu-Lakkala@users.noreply.github.com>
2022-01-29 17:48:09 +0200

    added movement change to enemy

--- Escalation_FGJ2022/Assets/StupidAI.cs
+++ Escalation_FGJ2022/Assets/StupidAI.cs
@@ -14,7 +14,6 @@ public class StupidAI : MonoBehaviour
 	public Transform attackPoint;
   
   public CharacterController2D controller;
-  public float reaction_dist = 1.5f;
   public float jumpRate = 1f;
   public float attackRate = 2f;
   public float attackRange = 0.5f;
@@ -23,11 +22,16 @@ public class StupidAI : MonoBehaviour
   
   float nextJumpTime = 0f;
   float nextAttackTime = 0f;
+  float nextMovementChange = 0f;
+  
+  //-1, 0, 1 based on wether we move away at or stay still
+  int movement_state;
+  
   
   float horizontalMove = 0f;
   bool jump = false;
   bool crouch = false;
-  float action_number;
+  
   // Update is called once per frame
   void Awake()
   {
@@ -36,6 +40,8 @@ public class StupidAI : MonoBehaviour
     Random.InitState(18);
     nextJumpTime = Random.Range(2, 20)/jumpRate;
     nextAttackTime = Random.Range(2, 20)/attackRate;
+    nextMovementChange = Random.Range(1, 8);
+    movement_state = (Random.Range(0, 3)-1);
   }
   
   
@@ -47,7 +53,6 @@ public class StupidAI : MonoBehaviour
       if(Time.time >= nextJumpTime)
       {
         {
-          Debug.Log("jumped");
           jump = true;
           nextJumpTime = (Time.time +Random.Range(2, 20)/jumpRate);
         }
@@ -57,19 +62,26 @@ public class StupidAI : MonoBehaviour
       if(Time.time >= nextAttackTime)
       {
         {
-          Debug.Log("attacked");
           Attack();
           nextAttackTime = (Time.time +Random.Range(2, 20)/attackRate);
         }
       }
       
+      //is it time to change movement_state
+      if(Time.time >= nextMovementChange)
+      {
+        movement_state = (Random.Range(0, 3)-1);
+        nextMovementChange = (Time.time +Random.Range(1, 8));
+        Debug.Log(movement_state);
+      }
+      
       
       // move towards player horizontaly
-      if((self.position.x - target.position.x)>reaction_dist)
+      if((self.position.x - target.position.x)>0)
       {
         horizontalMove = -1;
       }
-      else if ((self.position.x - target.position.x)<(-reaction_dist))
+      else if ((self.position.x - target.position.x)<0)
       {
         horizontalMove = 1;
       }
@@ -81,7 +93,7 @@ public class StupidAI : MonoBehaviour
    void FixedUpdate()
   {
     //move char
-    controller.Move(horizontalMove,crouch,jump);
+    controller.Move(horizontalMove*movement_state,crouch,jump);
     if (jump==true)
     {
       jump = false;
